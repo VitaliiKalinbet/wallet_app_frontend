55c7435f19057e9c8c78d8e8acd94e521bfff07a
dmb-2222 <dmb-2222@yandex.ru>
2020-01-12 21:03:19 +0200

    connect Loading to ReducerGlbal

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -10,6 +10,7 @@ const Loader = () => {
         color="#00BFFF"
         height={100}
         width={100}
+        z-index="999"
       />
     </div>
   );
--- src/redux/global/globalReducers.js
+++ src/redux/global/globalReducers.js
@@ -11,7 +11,19 @@ const isModalAddTransactionOpen = (state = false, { type }) => {
       return state;
   }
 };
+const loadingReducer = (state = false, { type }) => {
+  switch (type) {
+    case types.GLOBAL_FETCH_START:
+      return true;
+    case types.GLOBAL_FETCH_FINISH:
+    case types.GLOBAL_FETCH_ERROR:
+      return false;
+    default:
+      return state;
+  }
+};
 
 export default combineReducers({
   isModalAddTransactionOpen,
+  loading: loadingReducer,
 });
